26fa12f647b5b1e1ff32735b6894c1f0da57b4d9
freightdev <jesse.freightdev@gmail.com>
2026-01-03 22:23:17 -0500

    New Ranger Commit

--- .mark/devtool-script/agents/ssh-agent.zsh
+++
@@ -1,8 +0,0 @@
-##############
-# SSH AGENT
-##############
-
-
-if [[ -z "$SSH_AUTH_SOCK" && -f "$SSH_ID" ]]; then
-    eval "$(keychain --quiet --nogui --eval "$SSH_ID" 2>/dev/null)" >/dev/null 2>&1
-fi
--- .mark/devtool-script/setups/bun.zsh
+++
@@ -1,6 +0,0 @@
-# bun completions
-[ -s "/home/admin/.bun/_bun" ] && source "/home/admin/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
